65b6dba534f69bdab0d4b24460f05e4044a706d2
Mariana Teut <mary_marianailea@yahoo.com>
2019-11-14 00:41:17 +0200

    finish

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Pattern;
-
 
 
 public class Game {
@@ -20,13 +18,14 @@ public class Game {
     private List<Animal> availableAnimals = new ArrayList<>();
     private List<AnimalRescuer> availableRescuers = new ArrayList<>();
 
+
     public void start() throws Exception {
         System.out.println("***Welcome to AnimalRescuer***");
 
-        String selectedAnimalRescuer = getSelectedAnimalRescuerFromUser();
+        // String nameAnimalRescuer = getSelectedAnimalRescuerFromUser();
         initRescuer(1);
         initAnimal(1);
-        // initFoods();
+        //  initFoods();
         // initActivities();
         // displayAvailableFoods();
         // displayActivities();
@@ -35,7 +34,7 @@ public class Game {
         boolean winnerNotKnown = true;
         int roundsNumber = 1;
 
-        while (winnerNotKnown== true  && roundsNumber <= 3) {
+        while (winnerNotKnown == true && roundsNumber <= 3) {
             for (Animal animal : availableAnimals) {
                 System.out.println("Day " + roundsNumber + ":");
                 requireFeeding();
@@ -55,40 +54,39 @@ public class Game {
                 System.out.println();
             }
         }
-            if (winnerNotKnown == true)
+        if (winnerNotKnown == true)
 
-                System.out.println("Your animal is fed now and is happy, you won !");
+            System.out.println("Your animal is fed now and is happy, you won !");
 
     }
 
-    private String getSelectedAnimalRescuerFromUser()
-    {  System.out.println("Please enter your name for your rescuer:");
+    private String getSelectedAnimalRescuerFromUser() {
+        System.out.println("Please enter your name for your rescuer:");
         String name;
-        Scanner scanner=new Scanner(System.in);
-        name=scanner.nextLine();
-        if(Pattern.matches("[a-zA-z]+",name))
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        if (Pattern.matches("[a-zA-z]+", name))
             return name;
-        else{
+        else {
             System.out.println("Please enter a valid name!");
             return getSelectedAnimalRescuerFromUser();
         }
     }
 
     private void initRescuer(int rescuersCount) {
-        for (int i = 0; i < rescuersCount; i++) {
-            AnimalRescuer rescuer = new AnimalRescuer(getSelectedAnimalRescuerFromUser(), 2.00);
-            getAnimalFromUser();
-            System.out.println("Available Amount Of Money :" + String.format("%.0f", rescuer.getAmountOfMoney()));
-
-            availableRescuers.add(rescuer);
-        }
+        AnimalRescuer rescuer = new AnimalRescuer(getSelectedAnimalRescuerFromUser(), 2.00);
+        System.out.println("Hello " + rescuer.getName() + "!");
+        System.out.println("Your budget is: " + rescuer.getAmountOfMoney() + "$");
+        availableRescuers.add(rescuer);
+        getAnimalFromUser();
     }
+
     private String getAnimalFromUser() {
         System.out.println("Choose the animal you want to rescue (dog/cat) :");
         Scanner scanner = new Scanner(System.in);
         String animalFromUser = scanner.nextLine();
-        if ((Pattern.matches("[a-zA-z]+",animalFromUser))&&
-                (animalFromUser.equalsIgnoreCase("Cat")|animalFromUser.equalsIgnoreCase("Dog")) ) {
+        if ((Pattern.matches("[a-zA-z]+", animalFromUser)) &&
+                (animalFromUser.equalsIgnoreCase("Cat") | animalFromUser.equalsIgnoreCase("Dog"))) {
             System.out.println("You rescued : " + animalFromUser);
         } else {
             System.out.println("Please choose dog/cat !");
@@ -98,35 +96,31 @@ public class Game {
     }
 
     private String getAnimalNameFromUser() {
-        System.out.println("Please enter your "+getAnimalFromUser()+" 's name");
+        System.out.println("Please enter your animal 's name");
         String name;
-        Scanner scanner=new Scanner(System.in);
-        name=scanner.nextLine();
-        if(Pattern.matches("[a-zA-z]+",name))
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        if (Pattern.matches("[a-zA-z]+", name))
             return name;
-        else{
+        else {
             System.out.println("Please enter a valid name!");
             return getAnimalNameFromUser();
-
         }
-
     }
 
     private void initAnimal(int animalsCount) {
 
-        for (int i = 0; i < animalsCount; i++) {
-            Animal animal = new Dog(getAnimalNameFromUser());
-            animal.setAge(2.0);
-            animal.setLevelOfHeath(2);
-            animal.setLevelOfTheFeelingOfHunger(3);
-            animal.setFavoritFoodNames("purina");
-            animal.setFavoritRecreationalNames("run");
-            animal.setMoodLevel(6);
-            System.out.println();
+        // for (int i = 0; i < animalsCount; i++) {
+        Animal animal = new Dog(getAnimalNameFromUser());
+        animal.setAge(2.0);
+        animal.setLevelOfHeath(2);
+        animal.setLevelOfTheFeelingOfHunger(3);
+        animal.setFavoritFoodNames("purina");
+        animal.setFavoritRecreationalNames("run");
+        animal.setMoodLevel(6);
+        System.out.println();
 
-            availableAnimals.add(animal);
-
-        }
+        availableAnimals.add(animal);
     }
 
     private void requireFeeding() throws Exception {
@@ -177,17 +171,15 @@ public class Game {
     private void initFoods() throws Exception {
         int availfoodCount = getAvailFoodCountFromUser();
         System.out.println("Today's available foods is.");
-       for (int i = 0; i < availfoodCount; i++) {
+        for (int i = 0; i < availfoodCount; i++) {
+            AnimalFood food = new AnimalFood();
+            food.setName("pedigree");
+            availableFoods.add(food);
 
-           AnimalFood food = new AnimalFood();
-           food.setName("pedigree");
-           availableFoods.add(food);
-
-           AnimalFood food1 = new AnimalFood();
-           food1.setName("purina");
-           availableFoods.add(food1);
-       }
-
+            AnimalFood food1 = new AnimalFood();
+            food1.setName("purina");
+            availableFoods.add(food1);
+        }
     }
 
     private int getAvailFoodCountFromUser() throws Exception {
@@ -197,39 +189,40 @@ public class Game {
             return scanner.nextInt();
         } catch (InputMismatchException e) {
             throw new Exception("You entered an invalid number.");
+        }finally {
+
         }
     }
 
-    private void initActivities() {
-        RecreationActivity recreationActivity1 = new RecreationActivity("Run");
-        recreationActivity1.setRecreationActivityName("run");
-        availableActivities[0] = recreationActivity1;
+        private void initActivities () {
+            RecreationActivity recreationActivity1 = new RecreationActivity("Run");
+            recreationActivity1.setRecreationActivityName("run");
+            availableActivities[0] = recreationActivity1;
 
-        RecreationActivity recreationActivity2 = new RecreationActivity("game ball");
-        recreationActivity2.setRecreationActivityName("game ball");
-        availableActivities[1] = recreationActivity2;
+            RecreationActivity recreationActivity2 = new RecreationActivity("game ball");
+            recreationActivity2.setRecreationActivityName("game ball");
+            availableActivities[1] = recreationActivity2;
 
-    }
-
-    private void displayAvailableFoods() {
-        int count = 1;
-        System.out.println("Available animalFoods are:");
-        for (AnimalFood animalFood : availableFoods) {
-
-                System.out.println(count + "." + animalFood.getName());
-                count++;
-            }
-          //  System.out.println(count + ".I don't want to feed " + animal.getName() + " today.");
         }
-    //}
 
-
-    private void displayActivities() {
-        System.out.println("Available activities:");
-        for (int j = 0; j < availableActivities.length; j++) {
-            if (availableActivities[j] != null) {
-                System.out.println((j + 1) + ". " + availableActivities[j].getRecreationActivityName());
+        private void displayAvailableFoods () {
+            int count = 1;
+            System.out.println("Available animalFoods are:");
+            for (AnimalFood animalFood : availableFoods) {
+                if (availableFoods != null) {
+                    System.out.println(count + "." + animalFood.getName());
+                    count++;
+                } else {
+                    System.out.println(count + ".I don't want to feed " + animal.getName() + " today.");
+                }
             }
         }
+        private void displayActivities () {
+            System.out.println("Available activities:");
+            for (int j = 0; j < availableActivities.length; j++) {
+                if (availableActivities[j] != null) {
+                    System.out.println((j + 1) + ". " + availableActivities[j].getRecreationActivityName());
+                }
+            }
+        }
     }
-}
